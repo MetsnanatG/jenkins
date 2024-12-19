79f58b48a32c681226ee89c6b9dadbc4cedf8750
unknown <metsnanat.asfaw-ug@aau.edu.et>
2024-12-19 22:10:03 +0300

    add test with increase time

--- test.js
+++ test.js
@@ -3,7 +3,12 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('C:\\Users\\USER\\Documents\\CICD\\index.html'); // Path to your local HTML file
+    await page.goto('C:\\Users\\USER\\Documents\\CICD\\index.html',{
+
+        waitUntil: 'networkidle2',
+        timeout: 60000 // Increase timeout to 60 seconds
+
+    }); // Path to your local HTML file
     console.log('Page loaded successfully');
     await browser.close();
 })();
